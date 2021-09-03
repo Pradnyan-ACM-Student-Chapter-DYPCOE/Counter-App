964abd77decbdb8f10ef1a92ce6c77ac776d1e39
Adarsh Shete <shete.adarsh@gmail.com>
2021-09-03 10:26:14 +0530

    raw project-get started

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
-import React,{useState} from 'react';
-
-import './App.css'
-
+import React from 'react';
 
 function App() {
 
-  const [count,setCount]= useState(0);
-
-  const handleIncrement=()=>{
-  setCount(count+1);
-}
-  const handleDecrement=()=>{
-  setCount(count-1);
-}
-
   return (
     <div className='App'>
-    <div className="title">
-    COUNTER APP
-    </div>
-
-    <div className="count">{count}</div>
-    <br></br>
-    <button onClick={handleIncrement}>+</button> 
-    <button onClick={handleDecrement}>-</button> 
-    <div>
-    <span style={{margin:"2em",fontSize:"1.5em"}}>Increment</span>
-    <span style={{margin:"2em",fontSize:"1.5em"}}>Decrement</span>
-    </div>
     </div>
   );
 }
